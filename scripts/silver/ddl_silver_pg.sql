3e3db50ff8f0c0090e6a2b7dcb1f5af518e309ad
Ahmed Galall <145528713+adamgalall95@users.noreply.github.com>
2025-09-06 18:04:03 +0930

    Update and rename ddl_silver.sql to ddl_silver_pg.sql

--- scripts/silver/ddl_silver_pg.sql
+++ scripts/silver/ddl_silver_pg.sql
@@ -5,9 +5,10 @@ DDL Script: Create Silver Tables
 Script Purpose:
     This script creates tables in the 'silver' schema, dropping existing tables 
     if they already exist.
-	  Run this script to re-define the DDL structure of 'silver' Tables
+	  Run this script to re-define the DDL structure of 'bronze' Tables
 ===============================================================================
 */
+
 DROP TABLE IF EXISTS silver.crm_cust_info;
 CREATE TABLE IF NOT EXISTS silver.crm_cust_info (
     cst_id				INT,
